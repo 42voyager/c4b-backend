0e3bd757bdb8e2a533364582b33d911b5088333f
Microsoft Learn Student <learn@contoso.com>
2021-11-10 13:22:57 -0300

    linked with frontend

--- Startup.cs
+++ Startup.cs
@@ -53,7 +53,7 @@ namespace backend
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "backend v1"));
             }
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
 
             app.UseRouting();
 
