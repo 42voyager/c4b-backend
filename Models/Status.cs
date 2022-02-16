3c7ad0ab2984cc6a28d5fe3d5d4dd711624f82fa
Diego <diegocl02@gmail.com>
2022-02-16 11:49:17 -0500

    update

--- Models/Status.cs
+++ Models/Status.cs
@@ -2,8 +2,8 @@ namespace backend.Models
 {
 	static public class Status
 	{
-		static public string Credit { get { return "credit"; } }
-		static public string BankInfo { get { return "bankinfo"; } }
-		static public string Contract { get { return "contract"; } }
+		static public string Credit { get { return "Crédito Solicitado"; } }
+		static public string BankInfo { get { return "Dados Bancarios Enviados"; } }
+		static public string Contract { get { return "Contrato Assinado"; } }
 	}
 }
